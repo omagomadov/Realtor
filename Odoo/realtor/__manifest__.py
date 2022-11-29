b8da0ac3dffb19b9c1203760b14bdb07351f63a6
Oumar Magomadov <54516@etu.he2b.be>
2022-11-29 14:37:00 +0100

    Update realtor module

--- Odoo/realtor/__manifest__.py
+++ Odoo/realtor/__manifest__.py
@@ -3,21 +3,20 @@
     'name': "realtor",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+            """,
 
     'description': """
-        Long description of module's purpose
+        Vente d'appartements en ligne
     """,
 
-    'author': "My Company",
+    'author': "ESI",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Ventes',
+    'version': '1',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
@@ -25,11 +24,11 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+
+    'application' : True, 
 }
