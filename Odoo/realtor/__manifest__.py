cf148a02e40c1724806d9308c252aa1af4192631
Oumar Magomadov <54516@etu.he2b.be>
2022-11-30 02:12:49 +0100

    Add security file & update module information

--- Odoo/realtor/__manifest__.py
+++ Odoo/realtor/__manifest__.py
@@ -1,33 +1,33 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "realtor",
+    'name': "Realtor",
 
     'summary': """
+    Apartment sales
             """,
 
-    'description': """
-        Vente d'appartements en ligne
+    'description': """    
     """,
 
-    'author': "ESI",
+    'author': "Oumar Magomadov & Yacine Mamlouk",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Ventes',
-    'version': '1',
+    'category': 'Sales',
+    'version': '1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
     ],
     # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
+        #'demo/demo.xml',
     ],
 
     'application' : True, 
