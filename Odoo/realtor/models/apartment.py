65a19a42fb97a7709b3fbe485b9392211a286591
Oumar Magomadov <54516@etu.he2b.be>
2022-11-30 02:57:46 +0100

    Fixed compute function
    - Fixed 'error assign' for total surface column

--- Odoo/realtor/models/apartment.py
+++ Odoo/realtor/models/apartment.py
@@ -11,27 +11,28 @@ class Apartment(models.Model):
         ]
 
     #The name of the apartment (Unique name).
-    name = fields.Char(unique=True)
+    name = fields.Char(unique=True, string="Nom")
     #The description of the apartment.
-    description = fields.Text() 
+    description = fields.Text(string="Description") 
     #The image of the apartment.
-    image = fields.Image(max_height = 100, max_width = 100 ) 
+    image = fields.Image(max_height = 100, max_width = 100, string="Photo") 
     #The minimum date of availabilty for the apartment !TO-CHANGE!
-    available_date = fields.Date() 
+    available_date = fields.Date(string="Date de disponibilité") 
     #The price of the apartment.
-    price = fields.Integer()
+    price = fields.Integer(string="Prix")
     #The surface in m² of the apartment.
-    surface_apartment = fields.Integer()
+    surface_apartment = fields.Integer(string="Surface de l'appartement")
     #The surface in m² of the terrace.
-    surface_terrace = fields.Integer()
+    surface_terrace = fields.Integer(string="Surface de la terrasse")
     #The total surface of the apartment.
-    total_surface = fields.Integer(compute = '_calculate_total_surface')
+    total_surface = fields.Integer(compute = '_calculate_total_surface', string="Surface totale")
     #Buyer of the apartment - An apartment is bought by only one buyer.
-    buyer_id = fields.Many2one('res.partner', string='Buyer')
+    buyer_id = fields.Many2one('res.partner', string='Acheteur')
 
     #Computes the total surface according the surface apartment and the terrace surface.
     def _calculate_total_surface(self):
-        return self.surface_apartment + self.surface_terrace
+        for record in self :
+            record.total_surface = record.surface_apartment + record.surface_terrace
 
      
 
