a3168160df0d814adc3c35e6cd7c9af5e646caff
Oumar Magomadov <54516@etu.he2b.be>
2022-11-30 02:06:14 +0100

    Add module import in init

--- Odoo/realtor/models/apartment.py
+++ Odoo/realtor/models/apartment.py
@@ -1,13 +1,14 @@
 from odoo import models,fields
 
-
 class Apartment(models.Model):
     #The technical name.
-    _name = "apartment" 
+    _name = 'apartment' 
     #Check constraint for the price 
-    _sql_constraints = [('price_check','CHECK (price > 0)','The price must be greater than 0 ! '),
-    ('surface_apartement_check','CHECK(surface_apartment > 0)' ,'The surface of the apartment must be greater than 0 !'),
-    ('surface_terrace_check','CHECK(surface_terrace > 0)','The surface of the terrace must be greater than 0 !')]
+    _sql_constraints = [
+        ('price_check','CHECK (price > 0)','The price must be greater than 0 ! '),
+        ('surface_apartement_check','CHECK(surface_apartment > 0)' ,'The surface of the apartment must be greater than 0 !'),
+        ('surface_terrace_check','CHECK(surface_terrace > 0)','The surface of the terrace must be greater than 0 !')
+        ]
 
     #The name of the apartment (Unique name).
     name = fields.Char(unique=True)
@@ -16,25 +17,21 @@ class Apartment(models.Model):
     #The image of the apartment.
     image = fields.Image(max_height = 100, max_width = 100 ) 
     #The minimum date of availabilty for the apartment !TO-CHANGE!
-    available_date = fields.Date(default=date.today()) 
+    available_date = fields.Date() 
     #The price of the apartment.
     price = fields.Integer()
     #The surface in m² of the apartment.
     surface_apartment = fields.Integer()
     #The surface in m² of the terrace.
-    surface_terrace = fields.Integer(compute='_calculate_total_surface')
+    surface_terrace = fields.Integer()
     #The total surface of the apartment.
-    total_surface = fields.Integer()
+    total_surface = fields.Integer(compute = '_calculate_total_surface')
     #Buyer of the apartment - An apartment is bought by only one buyer.
-    buyer_id = fields.Many2one('res.partner',string='Buyer')
+    buyer_id = fields.Many2one('res.partner', string='Buyer')
 
-
-
-#Functions to compute attributes ...
-
-#Computes the total surface according the surface apartment and the terrace surface.
-def _calculate_total_surface(self):
-    return self.surface_apartment + self.surface_terrace
+    #Computes the total surface according the surface apartment and the terrace surface.
+    def _calculate_total_surface(self):
+        return self.surface_apartment + self.surface_terrace
 
      
 
