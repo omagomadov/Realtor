6a85f6bccb5cf493d48ce01152225eab3d534ad5
Oumar Magomadov <54516@etu.he2b.be>
2022-12-16 12:30:02 +0100

    Refractor code

--- Odoo/realtor/models/res_partner_offer.py
+++ Odoo/realtor/models/res_partner_offer.py
@@ -1,6 +1,7 @@
 from odoo import models, fields
 
-class Offer(models.Model) :
+
+class Offer(models.Model):
     _inherit = 'res.partner'
 
     apartment = fields.Many2one('apartment', string='Apartment', ondelete='cascade')
