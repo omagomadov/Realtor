ac333b9e63373ef3965d5f7950fc0c94b3a9b200
Oumar Magomadov <54516@etu.he2b.be>
2022-12-16 18:07:14 +0100

    Fix product price when choose apartement

--- Odoo/realtor/models/product_apartment.py
+++ Odoo/realtor/models/product_apartment.py
@@ -1,7 +1,13 @@
-from odoo import models, fields
+from odoo import models, fields, api
+import logging
 
 
 class ProductApartment(models.Model):
     _inherit = 'product.template'
 
-    apartment_product = fields.Many2one('apartment', string="Apartment", ondelete="cascade")
+    apartment_product = fields.Many2one('apartment', string="Apartment", ondelete="cascade", required=True)
+    list_price = fields.Float(compute='_product_price')
+
+    def _product_price(self):
+        for record in self:
+            record.list_price = record.apartment_product.price
