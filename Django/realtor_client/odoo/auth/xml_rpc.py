89fe8b09b149e66b6bd8eacd3f8444a8b99f9034
Oumar Magomadov <54516@etu.he2b.be>
2022-12-20 00:16:30 +0100

    Refractor xml-rpc script

--- Django/realtor_client/odoo/auth/xml_rpc.py
+++ Django/realtor_client/odoo/auth/xml_rpc.py
@@ -1,38 +1,7 @@
 import xmlrpc.client
-import getpass
 
 # Connection information
 url = 'http://localhost:8069'
-db = None
-username = None
-password = None
-
-
-# Display welcome message
-def welcome():
-    print("##########################################################")
-    print("#            |  __ \          | | |                      #")
-    print("#            | |__) |___  __ _| | |_ ___  _ __           #")
-    print("#            |  _  // _ \/ _` | | __/ _ \| '__|          #")
-    print("#            | | \ \  __/ (_| | | || (_) | |             #")
-    print("#            |_|  \_\___|\__,_|_|\__\___/|_|             #")
-    print("#                   XML-RPC script                       #")
-    print("##########################################################")
-
-
-# Asks credentials
-def ask_credential():
-    global username
-    global password
-    global db
-    try:
-        db = input("Enter your database : ")
-        username = input("Enter your login : ")
-        password = getpass.getpass("Enter your password : ")
-        connect(username, password, db)
-    except KeyboardInterrupt:
-        print("Exited")
-
 
 # Connects using given credential
 def connect(username, password, db):
@@ -41,16 +10,13 @@ def connect(username, password, db):
     try:
         uid = common.authenticate(db, username, password, {})
         if not uid:
-            print("Wrong credential")
-            ask_credential()
+            return False
         else:
-            print("Successfully connected")
-            search(db, uid, password)
+            return True
     except ConnectionRefusedError:
-        print("Cannot connect to localhost server")
+        return False
     except xmlrpc.client.Fault:
-        print("Cannot find database with the name : ", db)
-        ask_credential()
+        return False
 
 
 # Search an apartment
@@ -81,8 +47,3 @@ def search(db, uid, password):
             print("Cannot find an product with the apartment name", command)
         else:
             print("Exited")
-
-
-if __name__ == '__main__':
-    welcome()
-    ask_credential()
