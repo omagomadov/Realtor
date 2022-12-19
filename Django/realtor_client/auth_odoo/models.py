dbf4a6fb43279844f8880a929d269a5d6e9a96bf
Oumar Magomadov <54516@etu.he2b.be>
2022-12-20 00:18:53 +0100

    Define AbstractUser for User auth model

--- Django/realtor_client/auth_odoo/models.py
+++ Django/realtor_client/auth_odoo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = ['email']
+    REQUIRED_FIELDS = []
